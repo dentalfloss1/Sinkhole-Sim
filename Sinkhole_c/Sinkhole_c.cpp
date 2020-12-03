60178e8625ddebd75f7f54effa030a1342853b2c
Sarah Chastain <sarahichastain@gmail.com>
2020-12-03 17:53:13 -0500

    Nearly done with first attempt

--- Sinkhole_c/Sinkhole_c.cpp
+++ Sinkhole_c/Sinkhole_c.cpp
@@ -5,7 +5,7 @@
 #include <random>
 #include <algorithm>  
 #include <vector>
-
+#include <math.h>
 
 using namespace std;
 
@@ -20,18 +20,37 @@ void printlayer(vector<vector<int>> &layer);
 void labelgroups(vector<vector<int>> &baselayer);
 void groupcols(vector<vector<int>> &baselayer);
 void grouprows(vector<vector<int>> &baselayer);
+void computearea(vector<vector<int>> &baselayer, map<int, int> &area);
+void computewidth(vector<vector<int>> &baselayer, map<int, int> &width, map<int, int> &nextwidth);
+void computelength(vector<vector<int>> &baselayer, map<int, int> &length, map<int, int> &nextlength);
+double dp(double p, double gamma, int A);
+void updatelowerprob(vector<vector<double>> &baseprob, vector<vector<int>> &baselayer, map<int, double> &dpmap, double p);
+void updateupperprob(vector<vector<double>> &upperprob, vector<vector<int>> &baselayer, map<int, int> &area, map<int, int> &length, map<int, int> &width, double alpha);
 
 int main()
 {
 	vector<vector<int>> baselayer(N);
 	for (int i = 0; i < N; i++) baselayer[i] = vector<int>(M);
+	vector<vector<double>> baseprob(N);
+	for (int i = 0; i < N; i++) baseprob[i] = vector<double>(M);
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) baseprob[i][j] = 0;
+	vector<vector<double>> upperprob(N);
+	for (int i = 0; i < N; i++) upperprob[i] = vector<double>(M);
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) upperprob[i][j] = 0;
 
 	map<int, int> area;
+	area[0] = 0;
 	map<int, int> width;
+	width[0] = 1; //Defining special params for zero assists with calculations later (no 0/0 and no more ifs)
 	map<int, int> nextwidth;
 	map<int, int> length;
+	length[0] = 1;
 	map<int, int> nextlength;
-	double p = 0.4;
+	map<int, double> dpmap;
+
+	double p = 0.01;
+	double gamma = 0.06;
+	double alpha = 0.03;
 
 	initializebase(baselayer, p);
 	printlayer(baselayer);
@@ -42,66 +61,69 @@ int main()
 	grouprows(baselayer);
 	groupcols(baselayer);
 	
-	int lastholewidth = 0;
-	int largestwidth = 0;
-	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) {
-			if (area.find(baselayer[i][j]) == area.end()) area[baselayer[i][j]] = 1;
-			area[baselayer[i][j]]++;
-		}
-	}
-	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) {
-			if ((0 == baselayer[i][j - 1]) && (baselayer[i][j] != 0) && (0 == baselayer[i][j + 1])) {
-				if (nextwidth.find(baselayer[i][j]) == nextwidth.end()) nextwidth[baselayer[i][j]] = 1;
-				if (width.find(baselayer[i][j]) == width.end()) width[baselayer[i][j]] = 1;
+	computearea(baselayer, area);
+	computewidth(baselayer, width, nextwidth);
+	computelength(baselayer, length, nextlength);
 
-			}
-			else if ((0 != baselayer[i][j - 1]) && (0 == baselayer[i][j])) {
-				width[baselayer[i][j - 1]] = max(width[baselayer[i][j - 1]], nextwidth[baselayer[i][j - 1]]);
-				nextwidth[baselayer[i][j - 1]] = 0;
-			}
-			else if (baselayer[i][j] != 0) {
-				if (nextwidth.find(baselayer[i][j]) == nextwidth.end()) nextwidth[baselayer[i][j]] = 0;
-				if (width.find(baselayer[i][j]) == width.end()) width[baselayer[i][j]] = 0;
-				nextwidth[baselayer[i][j]]++;
-			}
-			
-		}
-	}
-	for (int j = 1; j < M - 1; j++) {
-		for (int i = 1; i < N - 1; i++) {
-			if ((0 == baselayer[i - 1][j]) && (baselayer[i][j] != 0) && (0 == baselayer[i + 1][j])) {
-				if (nextlength.find(baselayer[i][j]) == nextlength.end()) nextlength[baselayer[i][j]] = 1;
-				if (length.find(baselayer[i][j]) == length.end()) length[baselayer[i][j]] = 1;
-
-			}
-			else if ((0 != baselayer[i - 1][j]) && (0 == baselayer[i][j])) {
-				length[baselayer[i - 1][j]] = max(length[baselayer[i - 1][j]], nextlength[baselayer[i - 1][j]]);
-				nextlength[baselayer[i - 1][j]] = 0;
-			}
-			else if (baselayer[i][j] != 0) {
-				if (nextlength.find(baselayer[i][j]) == nextlength.end()) nextlength[baselayer[i][j]] = 0;
-				if (length.find(baselayer[i][j]) == length.end()) length[baselayer[i][j]] = 0;
-				nextlength[baselayer[i][j]]++;
-			}
-
-		}
-	}
 	printlayer(baselayer);
-				
+
+	
+	for (auto elem : area)
+	{
+		dpmap[elem.first] = dp(p, gamma, elem.second);
+	}
+	
+	cout << "area" << endl;
 	for (auto elem : area)
 	{
 		std::cout << elem.first << " " << elem.second << "\n";
+		dpmap[elem.first] = dp(p, gamma, elem.second);
 	}
+
+	cout << "width" << endl;
 	for (auto elem : width)
 	{
 		std::cout << elem.first << " " << elem.second << "\n";
 	}
+
+	cout << "length" << endl;
 	for (auto elem : length)
 	{
 		std::cout << elem.first << " " << elem.second << "\n";
 	}
+
+	cout << "dp" << endl;
+	for (auto elem : dpmap)
+	{
+		std::cout << elem.first << " " << elem.second << "\n";
+	}
+
+	updatelowerprob(baseprob, baselayer, dpmap, p);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) cout << baseprob[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+
+	updateupperprob(upperprob, baselayer, area, length, width, alpha);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) cout << upperprob[i][j] << " ";
+		cout << endl;
+	}
+	cout << endl;
+	random_device rd; //non-deterministic generator
+	mt19937 gen(rd());  // to seed mersenne twister
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			binomial_distribution<> dist(1, baseprob[i][j]); // distribute results between 1 and 6 inclusive.
+			baselayer[i][j] = dist(gen);
+		}
+	}
+
+	printlayer(baselayer);
+
     cout << "Hello World!\n";
 	return 0;
 }
@@ -170,8 +192,8 @@ void labelgroups(vector<vector<int>> &baselayer) {
 }
 
 void groupcols(vector<vector<int>> &baselayer) {
-	for (int j = 1; j < M; j++) {
-		for (int i = 1; i < N; i++) {
+	for (int j = 1; j < M-1; j++) {
+		for (int i = 1; i < N-1; i++) {
 			if ((0 != baselayer[i][j]) && (0 != baselayer[i - 1][j])) {
 				baselayer[i][j] = min(baselayer[i - 1][j], baselayer[i][j]);
 			}
@@ -185,7 +207,7 @@ void groupcols(vector<vector<int>> &baselayer) {
 }
 
 void grouprows(vector<vector<int>> &baselayer) {
-	for (int i = 1; i < N; i++) {
+	for (int i = 1; i < N-1; i++) {
 		for (int j = 1; j < M; j++) {
 			if ((0 != baselayer[i][j]) && (0 != baselayer[i][j + 1]) && (baselayer[i][j + 1] < baselayer[i][j])) {
 				for (int k = 0; k < M; k++) if (baselayer[i][k] == baselayer[i][j]) baselayer[i][k] = baselayer[i][j + 1];
@@ -193,6 +215,85 @@ void grouprows(vector<vector<int>> &baselayer) {
 			if ((0 != baselayer[i][j]) && (0 != baselayer[i][j - 1]) && (baselayer[i][j - 1] < baselayer[i][j])) {
 				for (int k = 0; k < M; k++) if (baselayer[i][k] == baselayer[i][j]) baselayer[i][k] = baselayer[i][j - 1];
 			}
+		}
+	}
+}
+
+void computearea(vector<vector<int>> &baselayer, map<int, int> &area) {
+	for (int i = 1; i < N - 1; i++) {
+		for (int j = 1; j < M - 1; j++) {
+			if ((area.find(baselayer[i][j]) == area.end()) && (0 != baselayer[i][j])) area[baselayer[i][j]] = 0;
+			if (0 != baselayer[i][j]) area[baselayer[i][j]]++;
+		}
+	}
+}
+
+
+void computewidth(vector<vector<int>> &baselayer, map<int, int> &width, map<int, int> &nextwidth) {
+
+	for (int i = 1; i < N - 1; i++) {
+		for (int j = 1; j < M - 1; j++) {
+			if ((0 == baselayer[i][j - 1]) && (baselayer[i][j] != 0) && (0 == baselayer[i][j + 1])) {
+				if (nextwidth.find(baselayer[i][j]) == nextwidth.end()) nextwidth[baselayer[i][j]] = 1;
+				if (width.find(baselayer[i][j]) == width.end()) width[baselayer[i][j]] = 1;
+
+			}
+			else if ((0 != baselayer[i][j - 1]) && (0 == baselayer[i][j])) {
+				width[baselayer[i][j - 1]] = max(width[baselayer[i][j - 1]], nextwidth[baselayer[i][j - 1]]);
+				nextwidth[baselayer[i][j - 1]] = 0;
+			}
+			else if (baselayer[i][j] != 0) {
+				if (nextwidth.find(baselayer[i][j]) == nextwidth.end()) nextwidth[baselayer[i][j]] = 0;
+				if (width.find(baselayer[i][j]) == width.end()) width[baselayer[i][j]] = 0;
+				nextwidth[baselayer[i][j]]++;
+			}
+
+		}
+	}
+
+}
+
+
+void computelength(vector<vector<int>> &baselayer, map<int, int> &length, map<int, int> &nextlength) {
+	for (int j = 1; j < M - 1; j++) {
+		for (int i = 1; i < N - 1; i++) {
+			if ((0 == baselayer[i - 1][j]) && (baselayer[i][j] != 0) && (0 == baselayer[i + 1][j])) {
+				if (nextlength.find(baselayer[i][j]) == nextlength.end()) nextlength[baselayer[i][j]] = 1;
+				if (length.find(baselayer[i][j]) == length.end()) length[baselayer[i][j]] = 1;
+
+			}
+			else if ((0 != baselayer[i - 1][j]) && (0 == baselayer[i][j])) {
+				length[baselayer[i - 1][j]] = max(length[baselayer[i - 1][j]], nextlength[baselayer[i - 1][j]]);
+				nextlength[baselayer[i - 1][j]] = 0;
+			}
+			else if (baselayer[i][j] != 0) {
+				if (nextlength.find(baselayer[i][j]) == nextlength.end()) nextlength[baselayer[i][j]] = 0;
+				if (length.find(baselayer[i][j]) == length.end()) length[baselayer[i][j]] = 0;
+				nextlength[baselayer[i][j]]++;
+			}
+
+		}
+	}
+}
+
+double dp(double p, double gamma, int A) {
+	double dpmax = (1.0 - p) / 4.0;
+	return dpmax * (1 - exp(-gamma * (double)A));
+}
+
+void updatelowerprob(vector<vector<double>> &baseprob, vector<vector<int>> &baselayer, map<int, double> &dpmap, double p) {
+	for (int i = 1; i < N - 1; i++) {
+		for (int j = 1; j < M - 1; j++) {
+			baseprob[i][j] = p + dpmap[baselayer[i - 1][j]] + dpmap[baselayer[i + 1][j]] + dpmap[baselayer[i][j - 1]] + dpmap[baselayer[i][j + 1]];
+		}
+	}
+}
+
+void updateupperprob(vector<vector<double>> &upperprob, vector<vector<int>> &baselayer, map<int, int> &area, map<int, int> &length, map<int, int> &width, double alpha) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			double r = ((double)area[baselayer[i][j]]) / ((double)length[baselayer[i][j]] + (double)width[baselayer[i][j]]);
+			upperprob[i][j] = 1 - exp(-alpha * r);
 		}
 	}
 }
